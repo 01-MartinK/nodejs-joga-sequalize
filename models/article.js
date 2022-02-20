dc36504d17212ba908e71544cc7a8104f34ae4b4
Martin K <thedeadgamer101@gmail.com>
2022-02-20 21:38:33 +0200

    Getted article author data

--- models/article.js
+++ models/article.js
@@ -11,7 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Author)
+      this.belongsTo(models.Author, {
+        foreignKey: {
+          name: 'AuthorId',
+          field: 'author_id',
+        }  
+      })
     }
   }
   Article.init({
