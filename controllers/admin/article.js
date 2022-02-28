e9cee62962446196571ad7f57312de1736b88af9
Martin K <thedeadgamer101@gmail.com>
2022-02-28 14:28:00 +0200

    Article Deletion

--- controllers/admin/article.js
+++ controllers/admin/article.js
@@ -27,6 +27,20 @@ const createArticle = (req, res) => {
     })
 };
 
+const deleteArticle = (req, res) => {
+	// get form data
+
+	models.Article.destroy({ where: { id: req.params.id } })
+	.then(article => {
+		console.log(article)
+		return res.status(200).json({ message: 'Article is deleted' });
+	})
+	.catch (error => {
+		return res.status(500).send(error.message);
+	})
+}
+
 module.exports = {
-    createArticle
+    createArticle,
+    deleteArticle
 };
